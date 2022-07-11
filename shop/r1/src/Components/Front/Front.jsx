19041e8d688a2fbbe4cb31f9f4b6f8622589c7e8
Arturito <arturaskisielius91@gmail.com>
2022-07-11 14:21:44 +0300

    Sort

--- shop/r1/src/Components/Front/Front.jsx
+++ shop/r1/src/Components/Front/Front.jsx
@@ -4,6 +4,7 @@ import Nav from './Nav';
 import { authConfig } from '../../Functions/auth';
 import axios from "axios";
 import FrontContext from './FrontContext';
+import SortFilter from './SortFilter';
 
 function Front() {
 
@@ -13,21 +14,22 @@ function Front() {
         useEffect(() => {
             axios
               .get("http://localhost:3003/products", authConfig())
-              .then((res) => setProducts(res.data));
+              .then((res) => setProducts(res.data.map((p, i) => ({...p, row: i}))));
           }, []);
 
     return (
         <FrontContext.Provider value={{
-            products
+            products,
+            setProducts
         }}>
         <Nav/>
         <div className="container">
                 <div className="row">
                     <div className="col-12">
-
+                        <SortFilter />
                     </div>
                     <div className="col-12">
-                        <List/>
+                        <List />
                     </div>
                 </div>
             </div>
